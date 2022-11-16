07889a235e55f61078cc240b3b00be1b005f99f3
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-11-16 09:22:19 +0000

    Added initial Invite User Page

---
+++ src/FamilyHub.IdentityServerHost/Areas/Identity/Pages/Account/InviteUserToCreateAccount.cshtml.cs
@@ -0,0 +1,185 @@
+using FamilyHub.IdentityServerHost.Models;
+using FamilyHub.IdentityServerHost.Models.Entities;
+using FamilyHub.IdentityServerHost.Persistence.Repository;
+using FamilyHub.IdentityServerHost.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NuGet.Configuration;
+using System.Data;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace FamilyHub.IdentityServerHost.Areas.Identity.Pages.Account;
+
+public class InviteUserToCreateAccountModel : PageModel
+{
+    private readonly IOrganisationRepository _organisationRepository;
+    private readonly IApiService _apiService;
+    private readonly UserManager<ApplicationIdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IConfiguration _configuration;
+    private readonly IEmailSender _emailSender;
+    public List<SelectListItem> OrganisationSelectionList { get; set; } = new List<SelectListItem>();
+
+    public List<IdentityRole> AvailableRoles { get; set; } = new List<IdentityRole>(); // default(List<IdentityRole>);
+
+    [BindProperty]
+    public string Email { get; set; } = default!;
+
+    [BindProperty]
+    public string RoleSelection { get; set; } = default!;
+
+    [BindProperty]
+    public int OrganisationNumber { get; set; } = 1;
+
+    [BindProperty]
+    public List<string> OrganisationCode { get; set; } = new List<string>();
+
+    [BindProperty]
+    public List<string>? Organisations { get; set; } = default!;
+
+    [BindProperty]
+    public bool ValidationValid { get; set; } = true;
+
+    [BindProperty]
+    public bool AllOrganisationsSelected { get; set; } = true;
+
+    [BindProperty]
+    public bool NoDuplicateOrganisations { get; set; } = true;
+
+    [BindProperty]
+    public int OrganisationNotSelectedIndex { get; set; } = -1;
+
+    [BindProperty]
+    public List<string> OrganisationSelectedByField { get; set; } = default!;
+
+    [BindProperty]
+    public List<string> DuplicateFoundByField { get; set; } = default!;
+
+    [BindProperty]
+    public string? ReturnUrl { get; set; }
+
+    public InviteUserToCreateAccountModel(IOrganisationRepository organisationRepository, IApiService apiService, UserManager<ApplicationIdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, IEmailSender emailSender)
+    {
+        _organisationRepository = organisationRepository;
+        _apiService = apiService;
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _configuration = configuration;
+        _emailSender = emailSender;
+    }
+
+    public async Task OnGetAsync(string? returnUrl = null)
+    {
+        ReturnUrl = returnUrl;
+        await InitPage();
+        if (!User.IsInRole("DfEAdmin"))
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            var user = await _userManager.FindByEmailAsync(userEmail);
+            var organisation = _organisationRepository.GetUserOrganisationIdByUserId(user.Id);
+            if (!string.IsNullOrEmpty(organisation))
+                OrganisationCode.Add(organisation);
+        }
+    }
+
+    private async Task InitPage()
+    {
+        if (User.IsInRole("DfEAdmin"))
+            AvailableRoles = _roleManager.Roles.OrderBy(x => x.Name).ToList();
+        else
+            AvailableRoles = _roleManager.Roles.Where(x => x.Name != "DfEAdmin").OrderBy(x => x.Name).ToList();
+
+        var list = await _apiService.GetListOpenReferralOrganisations();
+        OrganisationSelectionList = list.OrderBy(x => x.Name).Select(c => new SelectListItem() { Text = c.Name, Value = c.Id }).ToList();
+
+        if (Organisations != null)
+        {
+            OrganisationCode = Organisations;
+            OrganisationNumber = OrganisationCode.Count();
+        }
+        else
+        {
+            OrganisationCode.Add("Select organisation");
+            OrganisationNumber = OrganisationCode.Count;
+        }
+    }
+
+    public async Task OnPostAddAnotherOrganisation()
+    {
+        OrganisationCode.Add("Select organisation");
+        OrganisationNumber = OrganisationCode.Count;
+        await InitPage();
+    }
+
+    public async Task OnPostRemoveOrganisation(int id)
+    {
+        OrganisationCode.RemoveAt(id);
+        OrganisationNumber = OrganisationCode.Count;
+        await InitPage();
+    }
+
+    public async Task<IActionResult> OnPostNextPage()
+    {
+        if (!ModelState.IsValid)
+        {
+            ValidationValid = false;
+            await InitPage();
+            return Page();
+        }
+
+        if (Organisations != null)
+        {
+            for (int i = 0; i < Organisations.Count; i++)
+            {
+                if (Organisations[i] == null)
+                {
+                    OrganisationNotSelectedIndex = i;
+                    OrganisationNumber = Organisations.Count;
+                    ValidationValid = false;
+                    AllOrganisationsSelected = false;
+                    await InitPage();
+                    return Page();
+                }
+            }
+
+            for (int i = 0; i < Organisations.Count; i++)
+            {
+                for (int ii = 0; ii < Organisations.Count; ii++)
+                {
+                    if (Organisations[i] == Organisations[ii] && i != ii)
+                    {
+                        OrganisationNumber = Organisations.Count;
+                        ValidationValid = false;
+                        NoDuplicateOrganisations = false;
+                        OrganisationNotSelectedIndex = ii;
+                        await InitPage();
+                        return Page();
+                    }
+                }
+            }
+        }
+        var selected = string.Join(',', OrganisationCode ?? new List<string>());
+
+        var code = CreateAccountInvitationModel.GetTokenString(_configuration.GetValue<string>("InvitationKey"), selected, RoleSelection, DateTime.UtcNow.AddDays(1));
+
+        var callbackUrl = Url.Page(
+                    "/Account/ResetPassword",
+                    pageHandler: null,
+                    values: new { area = "Identity", code },
+                    protocol: Request.Scheme);
+
+        ArgumentNullException.ThrowIfNull(callbackUrl, nameof(callbackUrl));
+
+        await _emailSender.SendEmailAsync(
+                    Email,
+                    "Invitation to Create An Account",
+                    $"Please click to register an account (This link will expire in 24 hours) <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+
+        return RedirectToPage("./InviteUserSuccessful", new { email = Email, returnUrl = ReturnUrl });
+    }
+}
---
+++ src/FamilyHub.IdentityServerHost/Models/CreateAccountInvitationModel.cs
@@ -0,0 +1,39 @@
+﻿using FamilyHub.IdentityServerHost.Helpers;
+using FamilyHub.IdentityServerHost.Models.Entities;
+using FamilyHubs.ServiceDirectory.Shared.Models.Api.OrganisationType;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace FamilyHub.IdentityServerHost.Models;
+
+public class CreateAccountInvitationModel
+{
+    public CreateAccountInvitationModel()
+    {
+
+    }
+    public CreateAccountInvitationModel(string organisationId, string role, DateTime dateExpired)
+    {
+        OrganisationId = organisationId;
+        Role = role;
+        DateExpired = dateExpired;
+    }
+
+    public string OrganisationId { get; init; } = default!;
+    public string Role { get; init; } = default!;
+    public DateTime DateExpired { get; init; }
+
+    public static string GetTokenString(string key, string organisationId, string role, DateTime dateExpired)
+    {
+        CreateAccountInvitationModel createAccountInvitationModel = new CreateAccountInvitationModel(organisationId, role, dateExpired);
+        var content = Newtonsoft.Json.JsonConvert.SerializeObject(createAccountInvitationModel);
+        return Crypt.Encrypt(content, key); 
+    }
+
+    public static CreateAccountInvitationModel GetCreateAccountInvitationModel(string key, string tokenstring)
+    {
+        string json = Crypt.Decrypt(tokenstring, key);
+        return  JsonSerializer.Deserialize<CreateAccountInvitationModel>(tokenstring) ?? new CreateAccountInvitationModel();
+    }
+}
