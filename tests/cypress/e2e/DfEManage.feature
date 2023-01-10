a12ab3a1dad31237a88fc5ce945b49d39cae9fa0
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-01-10 10:35:34 +0000

    Completed Manage local authorities and voluntary community organisations Tests

--- tests/cypress/e2e/DfEManage.feature
+++ tests/cypress/e2e/DfEManage.feature
@@ -10,6 +10,11 @@ Scenario: DfE Admin logins and navigates to Manage LA and VCS
     When the DfE Admin clicks on the 'Manage local authorities and voluntary community organisations' link
     Then the DfE Admin is redirected to View Organisations Page
 
+Scenario: DfE Admin logins and navigates to Manage LA and VCS and then back
+    Given a DfE Admin logs in and goes to Manage local authorities and voluntary community organisations and then wants to go back
+    When the DfE Admin clicks on the ViewOrganisations back link
+    Then the DfE Admin is redirected back to the View Organisations Page
+
 Scenario Outline: DfEAdmin on the View Organisations Uses Filters
     Given a DfE Admin is on the View Organisations page
     When the DfE Admin selects the Filter
@@ -26,3 +31,26 @@ Scenario: DfE Admin logins and navigates to Manage LA and VCS
     When the DfE Admin clicks all the filters follwed by apply filter button
     Then presses clear filters link and the apply filter button, all results are shown
 
+Scenario: DfE Admin logins and navigates to Manage LA and VCS and Views and Item in the list
+    Given a DfE Admin logs in and navigates to Manage local authorities and voluntary community organisations page to view details
+    When the DfE Admin clicks on the Tower Hamlets View Link
+    Then the DfE Admin is redirect to the details page with the heading as 'Home-Start'
+
+Scenario: DfE Admin logins and navigates to Manage LA and VCS and Views and Item then goes back to previous page
+    Given a DfE Admin logs in and navigates to Manage local authorities and voluntary community organisations page to view item details
+    When the DfE Admin clicks on Back to manage local authorities and voluntary community organisations
+    Then the DfE Admin is redirect back to the Manage local authorities and voluntary community organisations page
+
+Scenario: DfE Admin logins and navigates to Manage LA and VCS and Views and Item then alters organisation name
+    Given a DfE Admin logs in and navigates to Manage local authorities and voluntary community organisations page then alters organisation name
+    When the DfEAdmin should modify the name to "<value>" and press save details
+    Then Local Authority name should be changed to "<value>"
+    Examples:
+        | value |
+        | Tower Hamlets CouncilX |
+        | Tower Hamlets Council |
+        
+Scenario: DfE Admin logins and navigates to Manage LA and VCS page then details page and then back
+    Given a DfE Admin logs in and goes to Manage local authorities and voluntary community organisations and then to the deatils page then wants to go back
+    When the DfE Admin clicks on the View Organisations Details page back link
+    Then the DfE Admin is redirected back to the Manage local authorities and voluntary community organisations Page
--- tests/cypress/e2e/DfEManage.js
+++ tests/cypress/e2e/DfEManage.js
@@ -29,6 +29,21 @@ Then("the DfE Admin is redirected to View Organisations Page", () => {
     cy.location('pathname').should('match', new RegExp("/Gds/Manage/ViewOrganisations"));
 });
 
+
+Given("a DfE Admin logs in and goes to Manage local authorities and voluntary community organisations and then wants to go back", () => {
+    cy.userservicelogin("DfEAdmin@email.com", "Pass123$");
+    cy.get('a[href*="/Gds/Manage/ViewOrganisations"]').click();
+});
+
+When("the DfE Admin clicks on the ViewOrganisations back link", (username, password) => {
+    cy.get('a[href*="/Gds/Manage/Homepage"]').click();
+});
+
+Then("the DfE Admin is redirected back to the View Organisations Page", () => {
+    cy.location('pathname').should('match', new RegExp("/Gds/Manage/Homepage"));
+});
+
+
 Given("a DfE Admin is on the View Organisations page", () => {
     cy.userservicelogin("DfEAdmin@email.com", "Pass123$")
     cy.get('a[href*="/Gds/Manage/ViewOrganisations"]').click();
@@ -74,3 +89,64 @@ Then("presses clear filters link and the apply filter button, all results are sh
     cy.get(".govuk-table").find("tr").find("td").contains("Local Authority");  
     cy.get('[type="checkbox"]').should('not.be.checked');
 });
+
+Given("a DfE Admin logs in and navigates to Manage local authorities and voluntary community organisations page to view details", () => {
+    cy.userservicelogin("DfEAdmin@email.com", "Pass123$")
+    cy.get('a[href*="/Gds/Manage/ViewOrganisations"]').click();
+});
+
+When("the DfE Admin clicks on the Tower Hamlets View Link", () => {
+    cy.get('a[href*="/Gds/Manage/ViewOrganisationDetail?id=88e0bffd-ed0b-48ea-9a70-5f6ef729fc21"]').click();
+});
+
+Then("the DfE Admin is redirect to the details page with the heading as {string}", (heading) => {
+    cy.get("h1").should("contain.text", heading);
+});
+
+Given("a DfE Admin logs in and navigates to Manage local authorities and voluntary community organisations page to view item details", () => {
+    cy.userservicelogin("DfEAdmin@email.com", "Pass123$")
+    cy.get('a[href*="/Gds/Manage/ViewOrganisations"]').click();
+});
+
+When("the DfE Admin clicks on Back to manage local authorities and voluntary community organisations", () => {
+    cy.get('a[href*="/Gds/Manage/ViewOrganisations"]').click();
+});
+
+Then("the DfE Admin is redirect back to the Manage local authorities and voluntary community organisations page", () => {
+    cy.location('pathname').should('match', new RegExp("/Gds/Manage/ViewOrganisations"));
+});
+
+
+
+
+Given("a DfE Admin logs in and navigates to Manage local authorities and voluntary community organisations page then alters organisation name", () => {
+    cy.userservicelogin("DfEAdmin@email.com", "Pass123$")
+    cy.get('a[href*="/Gds/Manage/ViewOrganisations"]').click();
+    cy.get('a[href*="/Gds/Manage/ViewOrganisationDetail?id=88e0bffd-ed0b-48ea-9a70-5f6ef729fc21"]').click();
+});
+
+When("the DfEAdmin should modify the name to {string} and press save details", (value) => {
+    cy.get('#Name').invoke('val', '');
+    cy.get('#Name').type(value);
+    cy.get('.govuk-button').click();
+});
+
+Then("Local Authority name should be changed to {string}", (value) => {
+
+    cy.get(".govuk-summary-list__value").contains(value);
+
+});
+
+Given("a DfE Admin logs in and goes to Manage local authorities and voluntary community organisations and then to the deatils page then wants to go back", () => {
+    cy.userservicelogin("DfEAdmin@email.com", "Pass123$");
+    cy.visit(`/Gds/Manage/ViewOrganisationDetail?id=88e0bffd-ed0b-48ea-9a70-5f6ef729fc21`);
+});
+
+When("the DfE Admin clicks on the View Organisations Details page back link", () => {
+    cy.get('.govuk-back-link').contains('Back').click();
+});
+
+Then("the DfE Admin is redirected back to the Manage local authorities and voluntary community organisations Page", () => {
+    cy.location('pathname').should('match', new RegExp("/Gds/Manage/ViewOrganisations"));
+});
+
